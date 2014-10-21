4500d63cbb2323da9ca8d869b22d9a178313fafc
unknown <karki.christina@gmail.com>
2014-10-20 20:44:13 -0500

    Controller,Service,Repository added

---
+++ ars/src/main/java/com/waa/ars/controller/RentalInfoController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.waa.ars.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author CKarki
+ */
+
+@Controller
+public class RentalInfoController {
+    
+}
---
+++ ars/src/main/java/com/waa/ars/service/impl/PictureServiceImpl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.waa.ars.service.impl;
+
+import com.waa.ars.service.ApartmentService;
+
+/**
+ *
+ * @author CKarki
+ */
+public class PictureServiceImpl implements ApartmentService{
+    
+}
